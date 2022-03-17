7e20613de1c129e0bd0dbaa8384d169e968e130d
unknown <joaovitor.abitante@gmail.com>
2022-03-17 17:24:01 -0300

    Update poo.py

--- poo.py
+++ poo.py
@@ -14,9 +14,15 @@ teste = []
 
 with open('caso01.txt', 'r') as arquivo:
    
-    teste = [Node(line[0], line[2:]) if line[1] != '/n' for line in arquivo.readlines()]
+   # teste = [Node(line[0], line[2:])  for line in arquivo.readlines() if line[1] != "/n"]
+    for line in arquivo.readlines():
+        
+        if line[2] != '\n':
+           teste.append(Node(line[0], line[2:]))
     
+   
 
-print(teste[2].sozinha)
+    for i in range(len(teste)):
+        print(teste[i].sozinha)
 
 
