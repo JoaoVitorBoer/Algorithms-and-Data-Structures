41397d522b78d74e800cc426bce8175c7f7305ad
unknown <joaovitor.abitante@gmail.com>
2022-03-17 18:56:29 -0300

    save

--- poo.py
+++ poo.py
@@ -4,20 +4,15 @@ class Node:
         self.sequencia = sequencia
 
 
-
-
-
 sozinhas = []
 sequencia = []
-teste = []
+vet = []
 
 with open('caso01.txt', 'r') as arquivo:
-    teste = [Node(line[0], line[2:-1])  for line in arquivo.readlines() if line[2] != "\n"]
+    vet = [Node(line[0], line[2:-1])  for line in arquivo.readlines() if line[2] != "\n"]
     
    
 
     
-    print(teste[1].sequencia)
-    print(teste[2].sequencia)
 
 
