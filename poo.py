9004e887a74b0538ad3cba1e19ba0faa1efd7fa2
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2022-03-20 20:39:11 -0300

    MY METHOD

--- poo.py
+++ poo.py
@@ -8,7 +8,7 @@ sozinhas = []
 sequencia = []
 vet = []
 
-with open('caso01.txt', 'r') as arquivo:
+with open('caso01', 'r') as arquivo:
     vet = [Node(line[0], line[2:-1])  for line in arquivo.readlines() if line[2] != "\n"]
     
    
