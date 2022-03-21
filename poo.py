5c3aab87dac166c45c1fb33efa80c52f157e4cd8
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2022-03-21 18:18:25 -0300

    Update poo.py

--- poo.py
+++ poo.py
@@ -1,19 +1,25 @@
 class Node:
-    def __init__(self, sozinha, sequencia):
+    def __init__(self, sozinha, sequencia, first):
         self.sozinha = sozinha
         self.sequencia = sequencia
+        self.first = first
 
 
 sozinhas = []
 sequencia = []
 vet = []
+salva = 0
+
 
 with open('caso01', 'r') as arquivo:
-    vet = [Node(line[0], line[2:-1])  for line in arquivo.readlines() if line[2] != "\n"]
+    vet = [Node(line[0], line[2:-1], True)  for line in arquivo.readlines() if line[2] != "\n"]
     
-   
-
-    #for i in range(len(vet)):
-    print(vet[22].sequencia) 
-
-    print(vet[22].sozinha)
+    for i in range(len(vet)):
+        for j in range(len(vet)):
+            if vet[j].sozinha in vet[i].sequencia:
+               vet[j].first = False
+             
+  
+  
+    for i in range(len(vet)):
+        print(vet[i].first)
