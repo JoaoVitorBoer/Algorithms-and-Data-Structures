b2239eb2bd6188f2a691f074e3718e28c1232924
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2022-03-23 00:06:28 -0300

    Update poo.py

--- poo.py
+++ poo.py
@@ -10,21 +10,24 @@ sequencia = []
 vet = []
 salva = 0
 
-def somador(inicial, vet):
+def somador(inicial, vet, soma_parcial):
+    soma = soma_parcial
     achou = True
-    for i in inicial: 
-        for j in range(len(vet)):    
-                if i == vet[j].sozinha:
-                    achou = False
-                    teste = inicial.replace(i, vet[j].sequencia)
-                    somador(teste, vet)
+    for i in inicial: #m e m i m o m u
+        for j in range(len(vet)): 
+           if i == vet[j].sozinha: #vv uu -> funcionando corretamente               
+               achou = False
+               soma = soma + len(vet[j].sequencia)
+               teste = inicial.replace(i, vet[j].sequencia)               
+               somador(teste, vet, soma)
+           
     if achou == True:
-      return inicial 
+      return soma
             
             
        
-with open('caso04', 'r') as arquivo:
-    vet = [Node(line[0], line[2:-1], True)  for line in arquivo.readlines() if line[0] != "\n"]
+with open('caso01', 'r') as arquivo:
+    vet = [Node(line[0], line[2:-1], True)  for line in arquivo.readlines() if line[2] != "\n"]
     
     for i in range(len(vet)):
         for j in range(len(vet)):
@@ -34,14 +37,12 @@ with open('caso04', 'r') as arquivo:
   
     for i in range(len(vet)):
         if vet[i].first == True:
-           salva2 = somador(vet[i].sequencia, vet)
+           salva2 = somador(vet[i].sequencia, vet, len(vet[i].sequencia))
+           
            break
-
-    soma = 0
-    for i in range(salva2):
-        soma = soma + 1
-
-    print(soma)
+    print(salva2)
+    
+    
             
 
 
