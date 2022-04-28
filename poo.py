7db7307d3cfb406cd9d3dd8687c7d9b6b9afb145
João Vitor Boer Abitante <joaovitor.abitante@gmail.com>
2022-04-28 10:16:01 -0300

    dsa

--- poo.py
+++ poo.py
@@ -9,51 +9,38 @@ class Node:
 sozinhas = []
 sequencia = []
 vet = []
-salva = 0
 
-def somador(inicial, vet):
+def Somador(vet, letra):
+  soma = 0
+  for i in range(len(vet)):
+    if vet[i].sozinha == letra:
+      if vet[i].sequencia == False:
+        return(1)
+      else:
+        for x in range(len(vet)):
+          for z in range(len(vet)):
+            soma += Somador(vet, vet[x].sequencia[z])
+  return soma      
+  
 
-    achou = True
-    for i in inicial: #m e m i m o m u
-        for j in range(len(vet)): 
-            if inicial == vet[j].sozinha: #vv uu -> funcionando corretamente 
-               if vet[j].tamanho == 0:              
-                achou = False
-                for x in vet[j].sequencia:
-                 soma = soma + somador(x, vet)
-                vet[j].tamanho = soma
-                
-            else:
-                achou = False
-                soma = vet[j].tamanho
-    if(achou == False):
-        soma = 1
-    return soma
+
+
             
             
        
 with open('caso01', 'r') as arquivo:
-    vet = [Node(line[0], line[2:-1], True, 0)  for line in arquivo.readlines() if line[2] != "\n"]
-    
+    vet = [Node(line[0], line[2:-1], True, int(len(line[2:-1])))  for line in  arquivo.readlines() if line[2] != "\n"]
+   
     for i in range(len(vet)):
         for j in range(len(vet)):
             if vet[j].sozinha in vet[i].sequencia:
                vet[j].first = False
-        
-    salva2 = 0
-    for i in range(len(vet)):
-        if vet[i].first == True:
-            salva2 +=  somador(vet[i].sequencia, vet)  
+      
+    soma = 0
+    for x in range(len(vet)):
+      if vet[x].first == True:
+        for j in range(len(vet[x].sequencia)):
+           soma += Somador(vet, vet[x].sequencia[j])
 
-                     
-    
-    print(salva2)
-    
-    
-            
-
-
-            
-    
-    
-        
+    print(soma)
+   
